6171f46af212c9cf00deb90cc31ee74b731264d5
maxwell.marcos <maxwell.marcos@ifood.com.br>
2023-05-31 10:47:45 +0100

    Updating branch

--- src/mlops_exercise/pipeline.py
+++ src/mlops_exercise/pipeline.py
@@ -1,65 +1,65 @@
-"""
-This is a boilerplate pipeline
-generated using Kedro 0.18.8
-"""
+# """
+# This is a boilerplate pipeline
+# generated using Kedro 0.18.8
+# """
 
-from typing import Dict
+# from typing import Dict
 
-from kedro.pipeline import Pipeline, node
+# from kedro.pipeline import Pipeline, node
 
-from mlops_exercise.pipelines import data_modeling as dm
-from mlops_exercise.pipelines import data_processing as dp
-from mlops_exercise.data_quality.nodes import check_ranges
-
-
-###########################################################################
-# Here you can find an example pipeline, made of two modular pipelines.
-#
-# Delete this when you start working on your own Kedro project as
-# well as pipelines/data_science AND pipelines/data_engineering
-# -------------------------------------------------------------------------
+# from mlops_exercise.pipelines import data_modeling as dm
+# from mlops_exercise.pipelines import data_processing as dp
+# from mlops_exercise.data_quality.nodes import check_ranges
 
 
-def create_pipelines(**kwargs) -> Dict[str, Pipeline]:
-    """Create the project's pipeline.
+# ###########################################################################
+# # Here you can find an example pipeline, made of two modular pipelines.
+# #
+# # Delete this when you start working on your own Kedro project as
+# # well as pipelines/data_science AND pipelines/data_engineering
+# # -------------------------------------------------------------------------
 
-    Args:
-        kwargs: Ignore any additional arguments added in the future.
 
-    Returns:
-        A mapping from a pipeline name to a ``Pipeline`` object.
+# def create_pipelines(**kwargs) -> Dict[str, Pipeline]:
+#     """Create the project's pipeline.
 
-    """
+#     Args:
+#         kwargs: Ignore any additional arguments added in the future.
 
-    data_processing_pipeline = dp.create_pipeline()
-    data_modeling_pipeline = dm.create_pipeline()
+#     Returns:
+#         A mapping from a pipeline name to a ``Pipeline`` object.
+
+#     """
+
+#     data_processing_pipeline = dp.create_pipeline()
+#     data_modeling_pipeline = dm.create_pipeline()
     
-     #### IMPLEMENT THIS ####
-    return {
-        "__default__": 
+#      #### IMPLEMENT THIS ####
+#     return {
+#         "__default__": 
 
-        Pipeline(
-            [
-                node(
-                    check_critical_nulls,
-                    inputs="",
-                    output=""
-                )
-            ]
-        ) +
+#         Pipeline(
+#             [
+#                 node(
+#                     check_critical_nulls,
+#                     inputs="",
+#                     output=""
+#                 )
+#             ]
+#         ) +
 
-        data_processing_pipeline +
+#         data_processing_pipeline +
 
-        Pipeline(
-            [
-                node(
-                    check_ranges,
-                    inputs="",
-                    outputs="",
-                )
-            ]
-        ) +
+#         Pipeline(
+#             [
+#                 node(
+#                     check_ranges,
+#                     inputs="",
+#                     outputs="",
+#                 )
+#             ]
+#         ) +
 
-        data_modeling_pipeline
-    }
-    #### IMPLEMENT THIS ####
+#         data_modeling_pipeline
+#     }
+#     #### IMPLEMENT THIS ####
